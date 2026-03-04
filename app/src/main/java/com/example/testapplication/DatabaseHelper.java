cccbe1b59ba1d72e8a8113886600f27c40146bd7
Christian Mediodia <christianmediodia27@gmail.com>
2026-03-04 21:18:37 +0800

    databaseStarter

---
+++ app/src/main/java/com/example/testapplication/DatabaseHelper.java
@@ -0,0 +1,10 @@
+package com.example.testapplication;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.Cursor;
+import android.content.ContentValues;
+import android.content.Context;
+public class DatabaseHelper {
+
+}
