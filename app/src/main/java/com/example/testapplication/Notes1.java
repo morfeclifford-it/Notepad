d9051dc33651ba577347ff58fa04313f9d71101a
Christian Mediodia <christianmediodia27@gmail.com>
2026-03-04 22:07:21 +0800

    adding backend in the notepad

--- app/src/main/java/com/example/testapplication/Notes1.java
+++ app/src/main/java/com/example/testapplication/Notes1.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class Notes1 extends AppCompatActivity {
 
     ImageButton backNote1;
+    ImageButton doneButton;
+    DatabaseHelper dbHelper;
+    TextView titleText, contentText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,34 @@ public class Notes1 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        doneButton = findViewById(R.id.doneButton);
+        titleText = findViewById(R.id.titleText);
+        contentText = findViewById(R.id.content);
+        dbHelper = new DatabaseHelper(this);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = titleText.getText().toString().trim();
+                String desc = contentText.getText().toString().trim();
+                String date = new SimpleDateFormat("MMM dd, yyyy", Locale.getDefault()).format(new Date());
+
+                if(title.isEmpty()){
+                    Toast.makeText(Notes1.this, "Please input a title", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                boolean saved = dbHelper.addUser(title, desc, date);
+
+                if(saved){
+                    Toast.makeText(Notes1.this, "The note had successfully saved", Toast.LENGTH_SHORT).show();
+                    finish();
+                } else {
+                    Toast.makeText(Notes1.this, "The note hadn't been saved", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
 
         backNote1 = findViewById(R.id.back);
         backNote1.setOnClickListener(
